c10d1219765eac654f6e388fdf223b7115dc7191
Krunal K <krunal@crowdanalytix.com>
2017-05-31 19:31:49 +0530

    Commited by kk1

--- Python/kk.py
+++ Python/kk.py
@@ -737,6 +737,55 @@ Sample Output
 import numpy as np
 a,b = map(int, input().split())
 print(np.eye(int(a),int(b),0))
+#################################################################################################
+Task
+
+You are given two arrays ( & ) of dimensions X. 
+Your task is to perform the following operations:
+
+Add ( + )
+Subtract ( - )
+Multiply ( * )
+Divide ( / )
+Mod ( % )
+Power ( ** )
+Input Format
+
+The first line contains two space separated integers,  and . 
+The next  lines contains  space separated integers of array . 
+The following  lines contains  space separated integers of array .
+
+Output Format
+
+Print the result of each operation in the given order under Task.
+
+Sample Input
+
+1 4
+1 2 3 4
+5 6 7 8
+Sample Output
+
+[[ 6  8 10 12]]
+[[-4 -4 -4 -4]]
+[[ 5 12 21 32]]
+[[0 0 0 0]]
+[[1 2 3 4]]
+[[    1    64  2187 65536]] 
+Use // for division in Python 3.
+
+
+##########################################CODE###################################################
+import numpy as np
+n, m = map(int, input().split())
+a, b = (np.array([input().split() for _ in range(n)], dtype=int) for _ in range(2))
+print(a+b, a-b, a*b, a//b, a%b, a**b, sep='\n') 
+
+
+
+
+
+
 
 
 By KK(krunal kalariya)
