d4d453bf696cdd45462eca64e228d07feecc361a
Krunal K <krunal@crowdanalytix.com>
2017-06-03 18:45:19 +0530

    Numpy added

--- Python/kk.py
+++ Python/kk.py
@@ -780,7 +780,233 @@ import numpy as np
 n, m = map(int, input().split())
 a, b = (np.array([input().split() for _ in range(n)], dtype=int) for _ in range(2))
 print(a+b, a-b, a*b, a//b, a%b, a**b, sep='\n') 
+#################################################################################################
+input array
 
+a= [8.5, 2.4, 3.1, 6.4, 5.5, 6.6, 7.7, 8.8, 9.9]
+
+Output array
+
+[ 8.  2.  3.  6.  5.  6.  7.  8.  9.]
+[  9.   3.   4.   7.   6.   7.   8.   9.  10.]
+[  8.   2.   3.   6.   6.   7.   8.   9.  10.]
+
+
+##########################################CODE###################################################
+
+import numpy as np
+#a = input().split()
+a= [8.5, 2.4, 3.1, 6.4, 5.5, 6.6, 7.7, 8.8, 9.9]
+kk = np.array(a, float)
+print(np.floor(kk), np.ceil(kk), np.rint(kk), sep="\n")
+
+#################################################################################################
+Task
+
+You are given a 2-D array with dimensions X. 
+Your task is to perform the  tool over axis  and then find the  of that result.
+
+Input Format
+
+The first line of input contains space separated values of  and . 
+The next  lines contains  space separated integers.
+
+Output Format
+
+Compute the sum along axis . Then, print the product of that sum.
+
+Sample Input
+
+2 2
+1 2
+3 4
+Sample Output
+
+24
+Explanation
+
+The sum along axis  = [ ] 
+The product of this sum = 
+
+############################################CODE###################################################
+
+import numpy as np
+n,m = map(int , input().split())
+kk = np.array([input().split() for _ in range(n)], int)
+print( np.prod(np.sum(kk, axis=0)))
+
+#################################################################################################
+Task
+
+You are given a 2-D array with dimensions X. 
+Your task is to perform the min function over axis  and then find the max of that.
+
+Input Format
+
+The first line of input contains the space separated values of  and . 
+The next  lines contains  space separated integers.
+
+Output Format
+
+Compute the min along axis  and then print the max of that result.
+
+Sample Input
+
+4 2
+2 5
+3 7
+1 3
+4 0
+Sample Output
+
+3
+Explanation
+
+The min along axis 1 =  
+The max of  = 
+############################################CODE###################################################
+
+import numpy as np
+n,m = map(int , input().split())
+kk = np.array([input().split() for _ in range(n)], int)
+print(np.max(np.min(kk, axis=1), axis=0))
+
+####################################################################################################
+mean
+
+The mean tool computes the arithmetic mean along the specified axis.
+
+import numpy
+
+my_array = numpy.array([ [1, 2], [3, 4] ])
+
+print numpy.mean(my_array, axis = 0)        #Output : [ 2.  3.]
+print numpy.mean(my_array, axis = 1)        #Output : [ 1.5  3.5]
+print numpy.mean(my_array, axis = None)     #Output : 2.5
+print numpy.mean(my_array)                  #Output : 2.5
+By default, the axis is None. Therefore, it computes the mean of the flattened array.
+
+var
+
+The var tool computes the arithmetic variance along the specified axis.
+
+import numpy
+
+my_array = numpy.array([ [1, 2], [3, 4] ])
+
+print numpy.var(my_array, axis = 0)         #Output : [ 1.  1.]
+print numpy.var(my_array, axis = 1)         #Output : [ 0.25  0.25]
+print numpy.var(my_array, axis = None)      #Output : 1.25
+print numpy.var(my_array)                   #Output : 1.25
+By default, the axis is None. Therefore, it computes the variance of the flattened array.
+
+std
+
+The std tool computes the arithmetic standard deviation along the specified axis.
+
+import numpy
+
+my_array = numpy.array([ [1, 2], [3, 4] ])
+
+print numpy.std(my_array, axis = 0)         #Output : [ 1.  1.]
+print numpy.std(my_array, axis = 1)         #Output : [ 0.5  0.5]
+print numpy.std(my_array, axis = None)      #Output : 1.11803398875
+print numpy.std(my_array)                   #Output : 1.11803398875
+By default, the axis is None. Therefore, it computes the standard deviation of the flattened array.
+
+Task
+
+You are given a 2-D array of size X. 
+Your task is to find:
+
+The mean along axis 
+The var along axis 
+The std along axis 
+Input Format
+
+The first line contains the space separated values of  and . 
+The next  lines contains  space separated integers.
+
+Output Format
+
+First, print the mean. 
+Second, print the var. 
+Third, print the std.
+
+Sample Input
+
+2 2
+1 2
+3 4
+Sample Output
+
+[ 1.5  3.5]
+[ 1.  1.]
+1.11803398875
+
+
+############################################CODE###################################################
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import numpy as np
+n,m = map(int, input().split())
+kk = np.array([input().split() for _ in range(n)], int)
+print(np.mean(kk, axis=1), np.var(kk, axis=0), np.std(kk, axis=None), sep="\n")
+###################################################################################################
+dot
+
+The dot tool returns the dot product of two arrays.
+
+import numpy
+
+A = numpy.array([ 1, 2 ])
+B = numpy.array([ 3, 4 ])
+
+print numpy.dot(A, B)       #Output : 11
+cross
+
+The cross tool returns the cross product of two arrays.
+
+import numpy
+
+A = numpy.array([ 1, 2 ])
+B = numpy.array([ 3, 4 ])
+
+print numpy.cross(A, B)     #Output : -2
+Task
+
+You are given two arrays  and . Both have dimensions of X. 
+Your task is to compute their matrix product.
+
+Input Format
+
+The first line contains the integer . 
+The next  lines contains  space separated integers of array . 
+The following  lines contains  space separated integers of array .
+
+Output Format
+
+Print the matrix multiplication of  and .
+
+Sample Input
+
+2
+1 2
+3 4
+1 2
+3 4
+Sample Output
+
+[[ 7 10]
+ [15 22]]
+##############################################CODE#################################################
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy as np
+n =int(input())
+kk1 = np.array([input().split() for _ in range(n)], int)
+kk2 = np.array([input().split() for _ in range(n)], int)
+print(np.dot(kk1,kk2))
 
 
 
